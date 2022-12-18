d17c3f11e7a873699ba13f10aeae8b7036bdfd3c
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-18 17:10:48 +0530

    [Adding Anagram Detection]

---
+++ Anagram Detection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Anagram_Detection
+{
+    public class Anagram
+    {
+        
+        static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+            // Console.WriteLine("Hello World!");
+            string FirstString, SecondString;
+            Console.WriteLine("Enter Two String for detect anagram :");
+            FirstString = Console.ReadLine();
+            SecondString = Console.ReadLine();
+            
+            
+                //Convert string to char array
+                char[] savechar1= FirstString.ToCharArray();
+                char[] savechar2 = SecondString.ToCharArray();
+
+                //sort the char array
+                Array.Sort(savechar1);
+                Array.Sort(savechar2);
+
+            string str1=new string(savechar1);
+            string str2=new string(savechar2);  
+                // if sorted char array are same then the string is anagram
+                
+                if ( str1.Equals(str2)) 
+                {
+
+                    Console.WriteLine(FirstString +   "and" +   SecondString +    "are anagram.");
+                }
+                else 
+                {
+                    Console.WriteLine(FirstString +   "and" +    SecondString +    "are not anagram.");
+                }
+
+
+
+
+
+        }
+    }
+}
