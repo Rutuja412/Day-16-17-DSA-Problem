aed36d1bd94f1db002e3334f86d4d263951f52da
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-18 18:22:18 +0530

    [Adding Bubble Sort]

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BubbleSort
+{
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = {7,6,9,4,3};
+            int temp;
+            Console.WriteLine("Given Array : ");
+            foreach (int a in arr)
+                Console.WriteLine(a + " ");
+            for (int i = 0; i <= arr.Length - 2; i++)
+            {
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                       temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.Write(  " Sorted array : ");
+            foreach (int a in arr)
+                Console.WriteLine(a + " ");
+                Console.ReadKey();
+
+        }
+    }
+}
+        
+    
+    
+
